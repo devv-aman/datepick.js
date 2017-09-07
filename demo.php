b711e3cba89948123ba951f8a0fd6c7511ee40ac
Foodinger <foodinger.in@gmail.com>
2017-09-07 11:10:29 +0530

    Project Initialization

---
+++ demo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>datepick.js Demo</title>
+</head>
+<body>
+   
+    <div class="calendar">
+        
+    </div>
+   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+</body>
+</html>
